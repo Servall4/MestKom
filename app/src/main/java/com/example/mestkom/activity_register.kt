5cff3697d26ff617ff98aa44200ed40d1a0928d1
Ivan <ugrumov.i@yandex.ru>
2023-01-19 21:43:11 +0300

    Ошибки полей ввода

--- app/src/main/java/com/example/mestkom/activity_register.kt
+++ app/src/main/java/com/example/mestkom/activity_register.kt
@@ -2,8 +2,10 @@ package com.example.mestkom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -11,11 +13,35 @@ class activity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        val login: EditText = findViewById(R.id.login)
-        val password: EditText = findViewById(R.id.password)
-        val confirmpassword: EditText = findViewById(R.id.confirmpassword)
-        val email: EditText = findViewById(R.id.email)
+        val loginEditText: EditText = findViewById(R.id.login)
+        val passwordEditText: EditText = findViewById(R.id.password)
+        val confirmpasswordEditText: EditText = findViewById(R.id.confirmpassword)
+        val emailEditText: EditText = findViewById(R.id.email)
         val createaccount: ImageButton = findViewById(R.id.loginbtn)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.error))
+
+
+
+        fun createAccount(){
+
+            var email: String = emailEditText.text.toString()
+            var login: String = loginEditText.text.toString()
+            var password: String = passwordEditText.text.toString()
+            var confirmpassword: String = confirmpasswordEditText.text.toString()
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                builder.setMessage(R.string.error)
+                return
+            }
+
+            if (password != confirmpassword){
+
+            }
+
+
+        }
+
 
 
 
