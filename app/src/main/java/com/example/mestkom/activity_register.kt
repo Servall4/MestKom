b0c714bd1d1a703062bf28577bc3aab0b3102aad
Ivan <ugrumov.i@yandex.ru>
2023-01-21 11:10:11 +0300

    Minor bug fixes

--- app/src/main/java/com/example/mestkom/activity_register.kt
+++ app/src/main/java/com/example/mestkom/activity_register.kt
@@ -3,11 +3,14 @@ package com.example.mestkom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.ThreadContextElement
 
 class activity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,10 @@ class activity_register : AppCompatActivity() {
         val confirmpasswordEditText: EditText = findViewById(R.id.confirmpassword)
         val emailEditText: EditText = findViewById(R.id.email)
         val createaccount: ImageButton = findViewById(R.id.loginbtn)
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle(resources.getString(R.string.error))
+        val loginerr: TextView = findViewById(R.id.loginerr)
+        val passworderr: TextView = findViewById(R.id.samepassworderr)
+        val emailerr: TextView = findViewById(R.id.emailerr)
+
 
 
 
@@ -31,13 +36,23 @@ class activity_register : AppCompatActivity() {
             var confirmpassword: String = confirmpasswordEditText.text.toString()
 
             if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                builder.setMessage(R.string.error)
+                emailerr.visibility = View.VISIBLE
                 return
-            }
+            } else
+                emailerr.visibility = View.INVISIBLE
 
             if (password != confirmpassword){
+                passworderr.visibility = View.VISIBLE
+                return
+            } else
+                passworderr.visibility = View.INVISIBLE
 
-            }
+            if (!login.matches(Regex("^(?=.*[A-Za-z0-9]\$)[A-Za-z][A-Za-z\\d.-]{0,19}\$"))){
+                loginerr.visibility = View.VISIBLE
+                return
+            } else
+                loginerr.visibility = View.INVISIBLE
+
 
 
         }
